fbbb70a8a222201d8414f1f731560f1e3bc696fd
Ferny1011 <fer-ney1011@outlook.com>
2025-03-26 12:53:13 -0300

    features finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,10 +28,9 @@ const Hero = () => {
     }
 
     useEffect(() => {
-        if(loadedVideos === totalVideos -1 ) {
+        if(loadedVideos === totalVideos - 1 ) {
             setIsLoading(false);
         }
-
     }, [loadedVideos]);
 
     useGSAP(()=> {
@@ -123,7 +122,7 @@ const Hero = () => {
                     G<b>A</b>MING
                 </h1>
                 <div className="absolute left-0 top-0 z-40 size-full">
-                    <div className="mt-1 px-5 sm:px-10">
+                    <div className="mt-24 px-5 sm:px-10">
                         <h1 className="special-font hero-heading text-blue-100">redefi<b>n</b>e</h1>
                         <p className="mb-5 max-w-64 font-robert-regular text-blue-100">Enter the Metagame Layer <br/> Unleash the Play Economy</p>
                         <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1" />
