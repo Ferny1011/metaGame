78c50aa612b793c2ba6c197b7686262483941a4b
Ferny1011 <fer-ney1011@outlook.com>
2025-03-28 13:02:43 -0300

    footer finished

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -56,7 +56,7 @@ const Features = () => {
                 <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
                     <BentoCard
                         src="videos/feature-1.mp4"
-                        title={<>radi<b>n</b>t</>}
+                        title={<>radia<b>n</b>t</>}
                         description="A cross-platform metagame app, turning your activities across web2 and web3 game into a rewarding adventure."
                     />
                 </BentoTilt>
